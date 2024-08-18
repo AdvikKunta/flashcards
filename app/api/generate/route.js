62911a2d931fbe0367205c96ffa1c792fb12c5ca
AdvikKunta <advikkunta@gmail.com>
2024-08-17 20:03:59 -0700

    followed tutorial, will make additional edits afterwards

---
+++ app/api/generate/route.js
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import Groq from "groq-sdk";
+
+const systemPrompt = `
+You are a flashcard creator, you take in text and create multiple flashcards from it. Make sure to create exactly 10 flashcards.
+Both front and back should be one sentence long.
+You should return in the following JSON format:
+{
+  "flashcards":[
+    {
+      "front": "Front of the card",
+      "back": "Back of the card"
+    }
+  ]
+}
+`;
+
+export async function POST(req) {
+  const groq = new Groq({ apiKey: process.env.NEXT_PUBLIC_GROQ_API_KEY });
+  const data = await req.text();
+  const completion = await groq.chat.completions.create({
+    messages: [
+      { role: "system", content: systemPrompt },
+      { role: "user", content: data },
+    ],
+    model: "llama3-8b-8192",
+    response_format: { type: "json_object" },
+  });
+
+  const flashcards = JSON.parse(completion.choices[0].message.content);
+  return NextResponse.json(flashcards.flashcards);
+}
